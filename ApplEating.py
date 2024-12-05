0ad606a5348d60e17d9e3fe28ffddc56111b43d6
Thundere <gabrielvaz1401@gmail.com>
2024-12-04 23:46:22 -0300

    1.1

--- ApplEating.py
+++ ApplEating.py
@@ -1,7 +1,6 @@
 # Base do jogo
 
 import pygame
-import time
 import random
 
 # init Pygame
@@ -45,7 +44,7 @@ def run_game():
     game_over = False
     game_close = False
 
-    x= width / 2
+    x = width / 2
     y = height / 2
 
     x_speed = 0
@@ -54,13 +53,13 @@ def run_game():
     snake_pixels = []
     snake_length = 1
 
-    target_x = round(random.randrange(0, width-snake_size) / 10.0) * 10.0
-    target_y = round(random.randrange(0, height-snake_size) / 10.0) * 10.0   
+    target_x = round(random.randrange(0, width - snake_size) / 10.0) * 10.0
+    target_y = round(random.randrange(0, height - snake_size) / 10.0) * 10.0   
 
     while not game_over:
 
         while game_close:
-            pygame.game.display.fill(preto)
+            game_display.fill(preto)
             game_over_message = message_font.render("Game Over!", True, vermelho)
             game_display.blit(game_over_message, [width / 3, height / 3])
             print_score(snake_length - 1)
@@ -76,9 +75,6 @@ def run_game():
                 if event.type == pygame.QUIT:
                     game_over = True
                     game_close = False
-                
-
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -96,38 +92,44 @@ def run_game():
                 if event.key == pygame.K_DOWN:
                     x_speed = 0
                     y_speed = snake_size
-            
+
+        # Verificando se a cobra saiu das bordas
         if x >= width or x < 0 or y >= height or y < 0:
             game_close = True
 
         x += x_speed
         y += y_speed
-        
 
+        # Atualizando o fundo e o alvo
         game_display.fill(preto)
         pygame.draw.rect(game_display, laranja, [target_x, target_y, snake_size, snake_size])
 
-        snake_pixels.append([x,y])
+        # Adicionando a nova posição da cabeça da cobra
+        snake_pixels.append([x, y])
 
         if len(snake_pixels) > snake_length:
             del snake_pixels[0]
-        
+
+        # Verificando se a cobra colidiu com ela mesma
         for pixel in snake_pixels[:-1]:
             if pixel == [x, y]:
                 game_close = True
-        
+
         draw_snake(snake_size, snake_pixels)
         print_score(snake_length - 1)
 
         pygame.display.update()
 
+        # Verificando se a cobra comeu a comida
         if x == target_x and y == target_y:
-            target_x = round(random,time.randrange(0, width-snake_size) / 10.0) * 10.0
-            target_y = round(random,time.randrange(0, height-snake_size) / 10.0) * 10.0
+            target_x = round(random.randrange(0, width - snake_size) / 10.0) * 10.0
+            target_y = round(random.randrange(0, height - snake_size) / 10.0) * 10.0
 
             snake_length += 1
 
-            clock.tick(snake_speed)
+        clock.tick(snake_speed)
 
     pygame.quit()
     quit()
+
+run_game()
